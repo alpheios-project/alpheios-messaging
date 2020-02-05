b6a7820db7f78648a1361fe7613024bebff0ba07
Kirlat <kirlat@yula.media>
2020-02-05 16:25:57 +0400

    Updated configuration

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import MessagingService from '@messServ/core/messaging-service.js'
 import WindowIframeDestination from '@messServ/destinations/window-iframe-destination.js'
 import RequestMessage from '@messServ/messages/request-message.js'
+import ResponseMessage from '@messServ/messages/response-message.js'
 import { CedictDestinationConfig } from '@messServ/configurations/destinations.js'
 
-export { MessagingService, WindowIframeDestination, RequestMessage, CedictDestinationConfig }
+export { MessagingService, WindowIframeDestination, RequestMessage, ResponseMessage, CedictDestinationConfig }
